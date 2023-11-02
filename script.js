61280bfef8e04533b3ff22e4ac61b79afda7c073
tillu-jay <149655578+tillu-jay@users.noreply.github.com>
2023-11-02 12:51:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#results");
+
+  if (height === "" || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please Give a Valid Height ${height}`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please Give a Valid Weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+    // show result
+    if (bmi < 18.6) {
+      results.innerHTML = `<span>${bmi} - You are Underweight</span>`;
+    } else if (bmi > 18.6 && bmi < 24.9) {
+      results.innerHTML = `<span>${bmi} - You are Normal</span>`;
+    } else {
+      results.innerHTML = `<span>${bmi} - You are Overweight</span>`;
+    }
+  }
+});
